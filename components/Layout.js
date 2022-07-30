f05222d684a244658504e2cacb0916a4a0b433aa
Alter80 <fahim.atul0@gmail.com>
2022-07-31 03:04:53 +0600

    added single product

---
+++ components/Layout.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + "- nextEcom" : "nextEcom"}</title>
+        <meta name="description" content="Ecommerce website using NextJs" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 justify-between shadow-md items-center px-4">
+            <Link href="/">
+              <a className="text-lg font-bold">nextEcom</a>
+            </Link>
+
+            <div>
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Copyright © 2022 nextEcom
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
