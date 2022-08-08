c88adb79716f55d93e2c65da803e39a423d1ff93
Alter80 <fahim.atul0@gmail.com>
2022-08-08 10:53:56 +0600

    cookies added

--- utils/Store.js
+++ utils/Store.js
@@ -1,9 +1,13 @@
 import { createContext, useReducer } from "react";
+import Cookies from "js-cookie";
 
 export const Store = createContext();
 
 const initialState = {
-  cart: { cartItems: [] },
+  // cart: { cartItems: [] },
+  cart: Cookies.get("cart")
+    ? JSON.parse(Cookies.get("cart"))
+    : { cartItems: [] },
 };
 
 function reducer(state, action) {
@@ -18,6 +22,7 @@ function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
 
@@ -26,6 +31,7 @@ function reducer(state, action) {
         (item) => item.slug !== action.payload.slug
       );
 
+      Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
     default:
